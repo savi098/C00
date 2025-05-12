c3d40114663c7c663380118013a5dc3ffebcb64e
savi098 <savijosue084@gmail.com>
2025-05-12 09:09:31 +0100

    check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -6,5 +6,5 @@ if [ -e "$fichier" ]; then
    else 
         echo " le fichier '$fichier' n'existe pas."
 fi 
-chmod -x check_file_exist.sh
+chmod +x check_file_exist.sh
 # ce code demande a l'utilisateur d'entrer le nom d'un fichier qu'il veut vérifier s'il existe ou pas .  
