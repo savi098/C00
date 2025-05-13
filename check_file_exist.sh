ba1d1f8769ede9878773603dd95d991676bea5dc
savi098 <savijosue084@gmail.com>
2025-05-13 19:31:06 +0100

    check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo " entrez le nom du fichier a verifier:"
-read fichier
-if [ -e "$fichier" ]; then
-    echo "le fichier '$fichier' existe."
+read filename
+if [ -f "$filename" ]; then
+    echo "le fichier '$filename' existe."
    else 
-        echo " le fichier '$fichier' n'existe pas."
+        echo " le fichier '$filename' n'existe pas."
 fi 
 chmod +x check_file_exist.sh
 # ce code demande a l'utilisateur d'entrer le nom d'un fichier qu'il veut vérifier s'il existe ou pas .  
