1815fe03ed97865ae2c4afc2f3461fb2654e8fd6
savi098 <savijosue084@gmail.com>
2025-05-24 14:40:14 +0100

    list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "voici la liste de tout les fichiers du repertoire courant"
-ls -la
+ls -l
 # le script  affiche tout les fichiers du repertoir courant
 # la commande ls permet de lister les fichiers courant et -la permet d'afficher tout les fichier y compris les caches
 
